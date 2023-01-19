a4a1f8f026d926b2546e29be10ced141af1bfbbc
saif-haseeb <saifhaseeb1995@gmail.com>
2023-01-19 23:01:05 +1100

    Updates

--- src/routes.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import About from './pages/About';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
