8163778fe0502591c1060b2229886539e6ae05b7
Saif Haseeb <s_haseeb811@hotmail.com>
2023-06-12 20:24:30 +1000

    Unecessary comments

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,11 +1,9 @@
 const header = {
-  // all the properties are optional - can be left empty or deleted
   homepage: 'https://saif-haseeb.github.io/saif-haseeb/',
   title: 'SH.',
 }
 
 const about = {
-  // all the properties are optional - can be left empty or deleted
   name: 'Saif Haseeb',
   role: 'Data Engineer',
   description:
@@ -18,23 +16,9 @@ const about = {
 }
 
 const projects = [
-  // projects can be added an removed
-  // if there are no projects, Projects section won't show up
-  /*
-  {
-    name: 'Project 1',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  }
-  */
 ]
 
 const skills = [
-  // skills can be added or removed
-  // if there are no skills, Skills section won't show up
   'Python',
   'SQL',
   'Java',
@@ -52,7 +36,6 @@ const skills = [
 ]
 
 const contact = {
-  // email is optional - if left empty Contact section won't show up
   email: 'saifhaseeb1995@gmail.com',
 }
 
