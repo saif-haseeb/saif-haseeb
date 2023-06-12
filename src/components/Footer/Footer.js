3dd59533e1d9ec44925ef19ab93269aadee49a1b
Saif Haseeb <s_haseeb811@hotmail.com>
2023-06-12 20:23:10 +1000

    portfolio changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/saif-haseeb'
+      href='https://github.com/saif-haseeb/saif-haseeb'
       className='link footer__link'
     >
       Created By Saif Haseeb
